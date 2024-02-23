752c29965b148bf00e945cfa68aef1b4b35fb267
Keshav Rathinavel <keshavr@Keshavs-MacBook-Pro.local>
2024-02-23 14:27:11 +0530

    Change updateQuality to include a range from 1 to 49

--- src/main/kotlin/GildedRose.kt
+++ src/main/kotlin/GildedRose.kt
@@ -20,7 +20,6 @@ private fun Item.updateForEach() {
         quality > 0 -> quality -= (if (name == "Sulfuras, Hand of Ragnaros") 0 else 1)
     }
 
-
     sellIn -= if (name == "Sulfuras, Hand of Ragnaros") 0 else 1
 
     if (sellIn < 0) {
@@ -28,12 +27,12 @@ private fun Item.updateForEach() {
             "Aged Brie" -> updateQuality()
             "Backstage passes to a TAFKAL80ETC concert" -> quality = 0
             "Sulfuras, Hand of Ragnaros" -> quality -= 0
-            else -> if (quality > 0) quality -= 1
+            else -> updateQuality(-1)
         }
     }
 }
 
 private fun Item.updateQuality(change: Int = 1) {
-    if (quality < 50) quality = (quality + change)
+    if (quality in 1..49) quality = (quality + change)
         .coerceIn(minimumValue = 0, maximumValue = 50)
 }
