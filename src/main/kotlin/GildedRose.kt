05a41cf4ceed7454736ff963167343f4030c7ae7
Keshav Rathinavel <keshavrathinavel@gmail.com>
2024-02-24 17:10:30 +0530

    refactor - extract updateQuality to a method

--- src/main/kotlin/GildedRose.kt
+++ src/main/kotlin/GildedRose.kt
@@ -10,35 +10,40 @@ class GildedRose(var items: List<Item>) {
 }
 
 private fun Item.updateForEach() {
-    updateSellInDays()
+    updateSellInDays().updateItemQuality()
+}
 
+private fun Item.updateItemQuality() {
     updateQuality(
         when (name) {
-        "Aged Brie" -> if (expired) 2 else 1
-        "Backstage passes to a TAFKAL80ETC concert" -> {
-            if (expired) -quality else when {
-                sellInDays < 5 -> 3
-                sellInDays < 10 -> 2
-                else -> 1
+            "Aged Brie" -> if (expired) 2 else 1
+            "Backstage passes to a TAFKAL80ETC concert" -> {
+                if (expired) -quality else when {
+                    sellInDays < 5 -> 3
+                    sellInDays < 10 -> 2
+                    else -> 1
+                }
             }
-        }
-        "Sulfuras, Hand of Ragnaros" -> 0
-        "Conjured Mana Cake" -> if (expired) -4 else -2
-        else -> if (expired) -2 else -1
+
+            "Sulfuras, Hand of Ragnaros" -> 0
+            "Conjured Mana Cake" -> if (expired) -4 else -2
+            else -> if (expired) -2 else -1
         }
     )
 }
 
 private val Item.expired get() = sellInDays < 0
 
-private fun Item.updateSellInDays() {
+private fun Item.updateSellInDays(): Item {
     sellInDays -= when (name) {
         "Sulfuras, Hand of Ragnaros" -> 0
         else -> 1
     }
+    return this
 }
 
-fun Item.updateQuality(change: Int = 1) {
+fun Item.updateQuality(change: Int = 1): Item {
     if (quality in 1..49) quality = (quality + change)
         .coerceIn(minimumValue = 0, maximumValue = 50)
+    return this
 }
