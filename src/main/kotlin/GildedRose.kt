406b77d5b84a6a3e5551e465c52a03991ef27d7d
Keshav Rathinavel <keshavrathinavel@gmail.com>
2024-02-24 13:31:13 +0530

    Refactor SellIn < 0

--- src/main/kotlin/GildedRose.kt
+++ src/main/kotlin/GildedRose.kt
@@ -17,18 +17,22 @@ private fun Item.updateForEach() {
             if (sellIn < 11) updateQuality()
             if (sellIn < 6) updateQuality()
         }
-        quality > 0 -> quality -= (if (name == "Sulfuras, Hand of Ragnaros") 0 else 1)
+        quality > 0 -> {
+            quality -= (if (name == "Sulfuras, Hand of Ragnaros") 0 else 1)
+        }
     }
 
     sellIn -= if (name == "Sulfuras, Hand of Ragnaros") 0 else 1
 
     if (sellIn < 0) {
-        when (name) {
-            "Aged Brie" -> updateQuality()
-            "Backstage passes to a TAFKAL80ETC concert" -> quality = 0
-            "Sulfuras, Hand of Ragnaros" -> quality -= 0
-            else -> updateQuality(-1)
-        }
+        updateQuality(
+            when(name) {
+                "Aged Brie" -> 1
+                "Backstage passes to a TAFKAL80ETC concert" -> -quality
+                "Sulfuras, Hand of Ragnaros" -> 0
+                else -> -1
+            }
+        )
     }
 }
 
